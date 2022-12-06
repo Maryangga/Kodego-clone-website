b481c18eb635d4f41d5bf312beaebafefa068676
Marianne Bueno <marianne08bueno@gmail.com>
2022-12-06 20:47:24 +0800

    Add devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require ('path')
 
 module.exports = {
     entry: "./app/assets/scripts/App.js",
@@ -6,12 +6,22 @@ module.exports = {
         filename: "bundled.js",
         path: path.resolve(__dirname, "app")
     },
-module: {
-    rules: [
-        {
-            test: /\.css$/i ,
+    devServer: {
+        before: function (app, server) {
+          server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+      },
+      mode: "development",
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
             use: ["style-loader" , "css-loader"]
-        }
-    ]
-}
+          }
+        ]
+      }
 }
